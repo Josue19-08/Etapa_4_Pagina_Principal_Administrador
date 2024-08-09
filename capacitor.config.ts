a75dfe92afd13ee1d46119655698ba75481f507e
Josué Araya Marín <104031367+Josue19-08@users.noreply.github.com>
2024-08-09 14:53:02 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ease_pagina_administrador',
+  webDir: 'www'
+};
+
+export default config;
